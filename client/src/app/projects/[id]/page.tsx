68b373f4353f6ecb743e29db017577107b29955a
Kaushallrai <kaushall.rai@gmail.com>
2024-12-04 16:16:51 +0545

    create new task modal

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -5,7 +5,7 @@ import Board from "../BoardView";
 import List from "../ListView";
 import Timeline from "../TimelineView";
 import Table from "../TableView";
-import ModalNewTask from "./ModalNewTask";
+import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
   params: { id: string };
@@ -15,12 +15,14 @@ const Project = ({ params }: Props) => {
   const { id } = params;
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
   return (
     <div>
-      {/* <ModalNewTask
+      <ModalNewTask
         isOpen={isModalNewTaskOpen}
-        onClose={() => setIsModalNewTaskOpen}
-      /> */}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      />
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,7 +1,7 @@
-import { X } from "lucide-react";
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "../Header";
+import { X } from "lucide-react";
 
 type Props = {
   children: React.ReactNode;
@@ -12,8 +12,9 @@ type Props = {
 
 const Modal = ({ children, isOpen, onClose, name }: Props) => {
   if (!isOpen) return null;
+
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4">
+    <div className="fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4">
       <div className="w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg dark:bg-dark-secondary">
         <Header
           name={name}
